27cb7bb6d7b2cc7c6ecd9ec618ed22b499437162
temp <temp@BS-N518-05>
2018-09-25 11:30:39 +0300

    second

---
+++ LabArray/LabArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(i);
+            }
+
+            foreach (int o in list)
+            {
+                if ((o%2) == 0)
+                {
+                    Console.WriteLine(o);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
