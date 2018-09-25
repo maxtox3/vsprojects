40f755b814a7cc57b0cac89a6aedb9c2e7bef5d0
temp <temp@BS-N518-05>
2018-09-25 12:06:06 +0300

    fourth

---
+++ LabStack/LabStack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack x = new Stack();
+            x.Push("a");
+            x.Push("b");
+            x.Push("d");
+            x.Push("g");
+            x.Push("j");
+            Console.WriteLine("Количество элемкентов до : " + x.Count);
+            Console.WriteLine(x.Peek());
+            Console.WriteLine(x.Pop());
+            Console.WriteLine(x.Pop());
+            Console.WriteLine(x.Pop());
+            Console.WriteLine("Количество элемкентов после : " + x.Count);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
