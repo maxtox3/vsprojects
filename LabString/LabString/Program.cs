b3d20685754e4f65bdbb2360cf432452372e17fa
temp <temp@BS-N518-05>
2018-09-25 13:27:04 +0300

    lab string

---
+++ LabString/LabString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello";
+            string world = " world!";
+            Console.WriteLine($"{hello}, {world}");
+            StringBuilder sb = new StringBuilder();
+            sb.Append(hello);
+            sb.Append(", ");
+            sb.Append(world);
+            Console.WriteLine(sb);
+            Console.WriteLine(String.Format("в {0} - {1:hh:mm}", world, DateTime.Now));
+            Console.ReadLine();
+        }
+    }
+}
