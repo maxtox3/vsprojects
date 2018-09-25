b930aaacbafae5f2dd2a080974aea76d9163e904
temp <temp@BS-N518-05>
2018-09-25 11:54:53 +0300

    third

---
+++ LabQueue/LabQueue/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            for( int i = 0; i < 100; i++)
+            {
+                queue.Enqueue(i);
+            }
+
+            Console.WriteLine("Размер очереди : {0}", queue.Count());
+            Console.ReadLine();
+
+            while (queue.Count > 0)
+            {
+                int x = queue.Dequeue();
+                if( x%2 == 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+
+            Console.WriteLine("Размер очереди : {0}", queue.Count());
+            Console.ReadLine();
+        }
+    }
+}
